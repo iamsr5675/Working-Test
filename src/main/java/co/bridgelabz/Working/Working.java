f57b9a434566d9e63e17526419e1f8fed4a0ae50
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-29 17:09:59 +0530

    Updated

--- src/main/java/co/bridgelabz/Working/Working.java
+++ src/main/java/co/bridgelabz/Working/Working.java
@@ -1,13 +1,37 @@
 package co.bridgelabz.Working;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class Working 
 {
-    public static void main( String[] args )
+	public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
-    }
+       long st=0;
+       long sp=0;
+       long time=0;
+
+       Scanner sc = new Scanner(System.in);
+      System.out.println("Press 0 to start time");
+      int n = sc.nextInt();
+      if( n == 0 )
+         { 
+             st = System.currentTimeMillis();
+         }
+       System.out.println("Press 1 to stop time");
+       int m=sc.nextInt();
+       if( m == 1 )
+         { 
+             sp = System.currentTimeMillis();
+         }
+     
+       System.out.println("Time duration between start and stop");
+         time = ( sp - st ) / 1000;
+       System.out.println(time+" is seconds");
+     
+
+       }
 }
