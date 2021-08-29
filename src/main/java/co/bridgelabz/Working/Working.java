e27f467de60d4db3c53d967fdf6094d971a16265
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-29 17:05:50 +0530

    Hello World

---
+++ src/main/java/co/bridgelabz/Working/Working.java
@@ -0,0 +1,13 @@
+package co.bridgelabz.Working;
+
+/**
+ * Hello world!
+ *
+ */
+public class Working 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
