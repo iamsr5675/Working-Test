87d6fc2da839e91a4981ff66f770f308a9ff21aa
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-29 17:31:18 +0530

    largest

--- src/main/java/co/bridgelabz/Working/Working.java
+++ src/main/java/co/bridgelabz/Working/Working.java
@@ -8,83 +8,21 @@ import java.util.Scanner;
  */
 public class Working 
 {
-	static int player1 = 0, player2 = 0, position = 0, winningPosition = 10, flag = 0, counter1 = 0,
-			counter2 = 0;
-
-	private static void playgame() {
-		if (flag == 0) {
-			counter1++;
-			int rollDie = rollCheck();
-			System.out.println("Value at die is: " + rollDie);
-			int checkOption = options();
-			switch (checkOption) {
-			case 0:
-				System.out.println("No play");
-				flag = 1;
-				break;
-			case 1:
-				System.out.println("Ladder");
-				player1 = player1 + rollDie;
-				if (player1 > winningPosition)
-					player1 = player1 - rollDie;
-				break;
-			case 2:
-				System.out.println("Snake");
-				player1 = player1 - rollDie;
-				if (player1 < 0)
-					player1 = 0;
-				break;
-			}
-			if (counter1 == winningPosition) {
-				System.out.println("Player1 is winner");
-			}
-			position++;
-
-		} else {
-			counter2++;
-			int rollDie = rollCheck();
-			System.out.println("Value at die is: " + rollDie);
-			int checkOption = options();
-			switch (checkOption) {
-			case 0:
-				System.out.println("No play");
-				flag = 0;
-				break;
-			case 1:
-				System.out.println("Ladder");
-				player2 = player2 + rollDie;
-				if (player2 > winningPosition)
-					player2 = player2 - rollDie;
-				break;
-			case 2:
-				System.out.println("Snake");
-				player2 = player2 - rollDie;
-				if (player2 < 0)
-					player2 = 0;
-				break;
-			}
-			if (counter2 == winningPosition) {
-				System.out.println("Player2 is winner");
-			}
-			position++;
-		}
-	}
-
-	private static int rollCheck() {
-		int rollValue = (int) (Math.floor(Math.random() * 10 % 6 + 1));
-		return rollValue;
-
-	}
-
-	private static int options() {
-		int option = (int) (Math.floor(Math.random() * 10 % 3));
-		return option;
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Game");
-		while (position != winningPosition) {
-			playgame();
-		}
-	}
-}
+	public class BasicCorePrograms 
+	{
+		
+		public static void main(String[] args)   
+		{  
+		int a, b, c, largest, temp;  
+		Scanner sc = new Scanner(System.in);  
+		System.out.println("Enter the first number:");  
+		a = sc.nextInt();  
+		System.out.println("Enter the second number:");  
+		b = sc.nextInt();  
+		System.out.println("Enter the third number:");  
+		c = sc.nextInt();    
+		temp=a>b?a:b;    
+		largest=c>temp?c:temp;    
+		System.out.println("The largest number is: "+largest);  
+		}  
+		}  
